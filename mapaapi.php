357af2606d90fd97e2304242bf0311c9a0c3fe55
byronjl2003 <byronjl2003@gmail.com>
2017-10-21 04:32:55 -0600

    prueba de Api

--- mapaapi.php
+++ mapaapi.php
@@ -57,7 +57,7 @@
 
     }
     //$varr = 'holismapaapi';
-    $query = $this->_conn->prepare("select cord1,cord2 from data2 where fecha between '2017-10-17' and '2017-10-18' limit 1");
+    $query = $this->_conn->prepare("select cord1,cord2 from data2 where fecha between '2017-10-17' and '2017-10-18' limit 3");
     $query->execute();
     $filas = $query->fetchAll(PDO::FETCH_ASSOC);
 
