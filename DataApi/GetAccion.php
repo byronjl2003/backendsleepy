5b2f89fc36a92f3c30923798177121cf8a200835
byronjl2003 <byronjl2003@gmail.com>
2017-11-13 23:57:30 -0600

    commit

--- DataApi/GetAccion.php
+++ DataApi/GetAccion.php
@@ -69,6 +69,7 @@
      $accionresp = $fila[1];
      $this->mostrarRespuesta($this->convertirJson($accionresp), 200);
 */
+$query->execute();
 $filas = $query->fetchAll(PDO::FETCH_ASSOC);
 $num = count($filas);
 if ($num > 0) {
